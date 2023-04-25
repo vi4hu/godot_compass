8bbd866fb824df3bc7005e95d72ea78dd2f6e418
vi4hu <rainkun3@protonmail.com>
2023-04-26 02:32:06 +0530

    custom array mesh

--- addons/compass/addon.gd
+++ addons/compass/addon.gd
@@ -6,9 +6,7 @@ var compass3d = preload("res://addons/compass/entities/compass3d.gd")
 
 func _enter_tree():
 	add_custom_type("Compass3D", "Node3D", compass3d, icon)
-	pass
 
 
 func _exit_tree():
 	remove_custom_type("Compass3D")
-	pass
--- addons/compass/entities/compass3d.gd
+++ addons/compass/entities/compass3d.gd
@@ -1,10 +1,42 @@
+@tool
 extends Node3D
 
-@onready var niddle:MeshInstance3D = $Niddle
 @export var north:Vector3 = Vector3.FORWARD
 @export var parent:Node
+@export var custom_container_resource : ArrayMesh :
+	set (value):
+		custom_container_resource = custom_container_resource_changed(value)
+@export var custom_niddle_resource : ArrayMesh :
+	set (value):
+		custom_niddle_resource = custom_niddle_resource_changed(value)
+
+var container_res = load("res://addons/compass/resources/container3d.mesh")
+var niddle_res = load("res://addons/compass/resources/niddle3d.mesh")
+var container: MeshInstance3D
+var niddle: MeshInstance3D
+
+func _init():
+	container = MeshInstance3D.new()
+	container.name = "Container"
+	container.mesh = container_res
+	add_child(container)
+	niddle = MeshInstance3D.new()
+	niddle.name = "Niddle"
+	niddle.mesh = niddle_res
+	add_child(niddle)
 
 
 func _physics_process(delta) -> void:
-	var new_rot:float = parent.rotation.y - north.y
-	niddle.set_rotation(Vector3(0, -new_rot, 0))
+	if parent:
+		var new_rot:float = parent.rotation.y - north.y
+		niddle.set_rotation(Vector3(0, -new_rot, 0))
+
+
+func custom_container_resource_changed(value: ArrayMesh) -> ArrayMesh:
+	container.mesh = value
+	return value
+
+
+func custom_niddle_resource_changed(value: ArrayMesh) -> ArrayMesh:
+	niddle.mesh = value
+	return value
