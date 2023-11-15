de4d220ebd37ca15ea56d737ad8e828bc904b89a
vi4hu <rainkun3@protonmail.com>
2023-11-15 18:42:39 +0530

    compassbar for 2d

--- addons/compass/entities/compassbar.gd
+++ addons/compass/entities/compassbar.gd
@@ -2,6 +2,8 @@
 extends TextureRect
 
 @export var parent: Node
+@export_enum("3D", "2D") var mode: String = "3D"
+@export var parent_property_for_current_direction: String = "rotation"
 @export_range(0.01, 0.5) var _lerp_speed: float = 0.1
 
 var default_bar_sprite = preload("res://addons/compass/resources/compassbar.png")
@@ -28,12 +30,24 @@ func _setup() -> void:
 
 
 func _physics_process(delta) -> void:
-	if parent:
-		if not parent.get("rotation") is Vector3:
-			set_physics_process(false)
-			print("WARNING: Parent 'rotation' Property doesn't have valid type, requires Vector3.")
-			return
-		var curr_val = material.get_shader_parameter("dir")
-		material.set_shader_parameter(
-			"dir", lerp_angle(curr_val, parent.rotation.y, _lerp_speed)
-		)
+		if parent:
+			match mode:
+				"3D":
+					if not parent.get("rotation") is Vector3:
+						set_physics_process(false)
+						print("WARNING: Parent 'rotation' Property doesn't have valid type, requires Vector3.")
+						return
+					var curr_val = material.get_shader_parameter("dir")
+					material.set_shader_parameter(
+						"dir", lerp_angle(curr_val, parent.rotation.y, _lerp_speed)
+					)
+
+				"2D":
+					if not parent.get(parent_property_for_current_direction) is float:
+						set_physics_process(false)
+						print("WARNING: Parent Property for current direction doesn't have valid type, requires float for mode 2D.")
+						return
+					var curr_val = material.get_shader_parameter("dir")
+					material.set_shader_parameter(
+						"dir", lerp_angle(curr_val, -parent.get(parent_property_for_current_direction), _lerp_speed)
+					)
